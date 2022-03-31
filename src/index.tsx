eafe19afd0bf570c0b2dcc04d36c629eacbcf3e5
Anton Detev <det-anton@yandex.ru>
2022-04-01 00:14:15 +0400

    feat: init project

---
+++ src/index.tsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import ReactDom from 'react-dom/client';
+import {
+    BrowserRouter,
+    Routes,
+    Route,
+    Link,
+} from 'react-router-dom';
+
+import About from './pages/about';
+import Home from './pages/home';
+
+const elem = document.getElementById('root')!;
+const root = ReactDom.createRoot(elem);
+
+root.render(
+    <BrowserRouter>
+        <Link to="/">home</Link>
+        <Link to="/about">about</Link>
+        <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="about" element={<About />} />
+        </Routes>
+    </BrowserRouter>,
+);
---
+++ webpack.config.prod.js
@@ -0,0 +1,29 @@
+/* eslint-disable @typescript-eslint/no-var-requires */
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+    mode: 'production',
+    entry: './src/index.tsx',
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: '[name].bundle.js',
+    },
+    module: {
+        rules: [
+            { test: /\.tsx?$/, loader: 'ts-loader', exclude: /node_modules/, }
+        ],
+    },
+    resolve: {
+        extensions: ['.tsx', '.ts', '.js'],
+    },
+    plugins: [
+        new HtmlWebpackPlugin({ template: './assets/index.html' })
+    ],
+    optimization: {
+        runtimeChunk: 'single',
+        splitChunks: {
+            chunks: 'all'
+        }
+    }
+};
